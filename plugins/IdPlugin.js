cc551936fcf75a88d5f8928d743f3e8c680a147b
Tobias Beer <beertobias@gmail.com>
2013-03-07 18:52:27 +0100

    IdPlugin v.0.9 - provides store.tiddlerId and store.getTiddlerById

--- plugins/IdPlugin.js
+++ plugins/IdPlugin.js
@@ -1,58 +1,74 @@
 /***
 |''Name:''|IdPlugin|
-|''Description:''|generates and retrieves unique ids for tiddlers|
+|''Description:''|» provides {{{store.tiddlerId(tiddlerOrTitle)}}} to persist and retrieve unique tiddlers ids<br>» provides {{{store.getTiddlerById(id)}}} to retrieve tiddlers by their id |
 |''Documentation:''|http://id.tiddlyspace.com|
 |''Author:''|Tobias Beer / Mario Pietsch|
-|''Version:''|0.1|
+|''Version:''|1.0|
 |''Source''|https://raw.github.com/tobibeer/TiddlyWikiPlugins/master/plugins/IdPlugin.js|
 |''License''|[[Creative Commons Attribution-Share Alike 3.0|http://creativecommons.org/licenses/by-sa/3.0/]]|
+|''Implements''|http://www.broofa.com/Tools/Math.uuid.js|
 ***/
 //{{{
 
 /* allow for a specific prefix */
-config.extensions.id = '';
+config.extensions.idPrefix = '';
 
 /* retrieves a tiddler id or creates on if not existing */
 TiddlyWiki.prototype.tiddlerId = function (tiddler) {
-    if (typeof tiddler != String)
+    if (typeof tiddler != 'string')
         tiddler = tiddler.title;
     var id = store.getValue(tiddler, 'id');
-    if (!id)
-        store.setValue(tiddler,'id', config.extensions.id + Math.uuid(18) );
+    if (!id) {
+        id = config.extensions.idPrefix + Math.uuid(18);
+        store.setValue(tiddler, 'id', id);
+    }
+    return id;
 }
 
 /* retrieves a tiddler by its id */
 TiddlyWiki.prototype.getTiddlerById = function (id) {
-    store.forEachTiddler(function(tid){
-        if(store.getValue(tid.title,'id') == id)
-            return tid;
+    var t;
+    store.forEachTiddler(function (title, tiddler) {
+        if (id === store.getValue(title, 'id') ) {
+            t = tiddler;
+            return false;
+        }
     });
-}
+    return t;
+};
 
 
-/* id generation below */
+/*!
+Shortened version by PMario (1.0 - 2011.05.22) of...
+http://www.broofa.com/Tools/Math.uuid.js
 
-(function() {
-    /* array of chars to use */
-    var CHARS = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'.split(''); 
+Math.uuid.js (v1.4)
+http://www.broofa.com
+mailto:robert@broofa.com
 
-    /* define uuid function */
+Copyright (c) 2010 Robert Kieffer
+Dual licensed under the MIT and GPL licenses.
+*/
+(function () {
+    // Private array of chars to use
+    var CHARS = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'.split('');
+
     Math.uuid = function (len, radix) {
         var chars = CHARS, uuid = new Array(36);
         radix = radix || chars.length;
 
         if (len) {
             // Compact form
-            for (var i = 0; i < len; i++) uuid[i] = chars[0 | Math.random()*radix];
+            for (var i = 0; i < len; i++) uuid[i] = chars[0 | Math.random() * radix];
         } else {
-            var rnd=0, r;
+            var rnd = 0, r;
             for (var i = 0; i < 36; i++) {
-                if (i==8 || i==13 ||  i==18 || i==23) {
+                if (i == 8 || i == 13 || i == 18 || i == 23) {
                     uuid[i] = '-';
-                } else if (i==14) {
+                } else if (i == 14) {
                     uuid[i] = '4';
                 } else {
-                    if (rnd <= 0x02) rnd = 0x2000000 + (Math.random()*0x1000000)|0;
+                    if (rnd <= 0x02) rnd = 0x2000000 + (Math.random() * 0x1000000) | 0;
                     r = rnd & 0xf;
                     rnd = rnd >> 4;
                     uuid[i] = chars[(i == 19) ? (r & 0x3) | 0x8 : r];
@@ -62,5 +78,6 @@ TiddlyWiki.prototype.getTiddlerById = function (id) {
 
         return uuid.join('');
     };
+
 })();
 //}}}
