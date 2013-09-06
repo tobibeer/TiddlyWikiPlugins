c65bf7a16b38d27c3cd684a844b67082b66bde7e
Tobias Beer <beertobias@gmail.com>
2013-09-06 18:27:07 +0200

    Onload — re-added tscan for @followers and "What others say..."
    ...previously rendered via @evalify expressions, now using
    InlineJavascriptPlugin

--- resources/tobibeer/OnLoad.js
+++ resources/tobibeer/OnLoad.js
@@ -1,7 +1,13 @@
 <script>
 (function($){
-	$tid = $(place).closest('.tiddler');
-	tid = store.getTiddler($tid.attr('tiddler'));
+	var
+		$tid = $(place).closest('.tiddler'),
+		$v = $tid.find('.viewer'),
+		tid = store.getTiddler($tid.attr('tiddler')),
+		run = $tid.attr('runOnce');
+
+	if(run)return;
+	else $tid.attr('runOnce',true);
 
 	if(!tid)return;
 
@@ -11,6 +17,22 @@
 
 	if(tid.tags && tid.tags.contains('no-tags')){
 	    $('.infoTags',$tid).hide();
+	}
+	
+	if(tid.tags && tid.tags.contains('follow')){
+		wikify(
+			'{{followersFollowers{\n!This users followers...\n'+
+			'<<followers "' + tid.title + '" fat:n sort:server.bag hide:tobibeer>>}}}',
+			( $('<div/>').appendTo($v) )[0]
+		)
+	}
+
+	if(tid.tags && tid.tags.containsAny(['Talks','WhatOthersSay'])){
+		wikify(
+			'{{tbScan{\n!What everyone says right now...\n'+
+			'<<tsScan "' + tid.title + '" fat:y template:Templates##TALK sort:server.bag hide:tobibeer>>\n}}}',
+			( $('<div/>').appendTo($v) )[0]
+		)
 	}
 })(jQuery)
 </script>
