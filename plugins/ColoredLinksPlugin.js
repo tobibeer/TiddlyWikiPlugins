db8a1667a59d9b0773a284cf183a65161f81feaf
Tobias Beer <beertobias@gmail.com>
2013-09-26 23:41:46 +0200

    ColoredLinksPlugin v.1.1.0 — minor update, jQuery => $

--- plugins/ColoredLinksPlugin.js
+++ plugins/ColoredLinksPlugin.js
@@ -2,23 +2,30 @@
 |''Name''|ColoredLinksPlugin|
 |''Description''|adds color to tiddlyLinks depending on the tag assigned to the corresponding tiddler|
 |''Documentation''|http://ColoredLinks.tiddlyspot.com|
-|''Version''|1.0.3 (2010-09-30)|
+|''Version''|1.1.0 (2013-09-25)|
 |''Author''|[[TobiasBeer]]|
+|''CoreVersion''|2.5.2|
 |''Source''|https://raw.github.com/tobibeer/TiddlyWikiPlugins/master/plugins/ColoredLinksPlugin.js|
 |''License''|[[Creative Commons Attribution-Share Alike 3.0|http://creativecommons.org/licenses/by-sa/3.0/]]|
 !Code
 ***/
 //{{{
-config.extensions.coloredLinks = {
+(function($){
+
+var me = config.extensions.coloredLinks = {
     colorTags: true,
     colorTitles: true,
     titleSelector: 'div.title',
     excludeTagged: 'noColor',
 
     color: function (btn, title, takeTitle) {
-        var bd, c = config.options, e = btn, f, l = '', t, ta, ti = '', tg;
-        if (takeTitle) ti = title;
-        else {
+        var bd, f, l = '', t, ta, ti = '', tg,
+            e = btn,
+            c = config.options;
+
+        if (takeTitle) {
+            ti = title;
+        } else {
             do {
                 ti = e && e.getAttribute ? e.getAttribute('tiddler') : null;
                 l = ti ? e : l;
@@ -49,17 +56,17 @@ config.extensions.coloredLinks = {
             f = f.replace(/\s*;\s*/g, '","').replace(/\s*:\s*/g, '":"').trim();
             if (f.substr(f.length - 2, 2) == ',"') f = f.substr(0, f.length - 2);
             f = '{"' + f + '}';
-            jQuery(btn).css(jQuery.parseJSON(f)).addClass('coloredLink');
+            $(btn).css($.parseJSON(f)).addClass('coloredLink');
         }
         //always assign firsttag accordingly
-        if (tg) jQuery(btn).attr('firsttag', tg);
+        if (tg)$(btn).attr('firsttag', tg);
     }
 }
 
 createTiddlyLink_COLOR = createTiddlyLink;
 createTiddlyLink = function (place, title, includeText, className, isStatic, linkedFromTiddler, noToggle) {
     var b = createTiddlyLink_COLOR.apply(this, arguments);
-    config.extensions.coloredLinks.color(b, title);
+    me.color(b, title);
     return b;
 }
 
@@ -67,27 +74,42 @@ createTagButton_COLOR = createTagButton;
 createTagButton = function (place, tag, excludeTiddler, title, tooltip) {
     var b = createTagButton_COLOR.apply(this, arguments);
     addClass(b, store.getTaggedTiddlers(tag).length > 0 ? 'hastags' : 'hasnotags');
-    config.extensions.coloredLinks.color(b, tag);
+    me.color(b, tag);
     return b;
 }
 
 config.macros.allTags.handler_COLOR = config.macros.allTags.handler;
 config.macros.allTags.handler = function (place, macroName, params) {
     config.macros.allTags.handler_COLOR.apply(this, arguments);
-    jQuery('.button, .tiddlyLink', place).each(function (i) {
-        var btn = jQuery(this), title = btn.attr('tiddlyLink');
-        if (!title && btn.hasClass('hastags') || btn.hasClass('hasnotags')) title = btn.attr('tag');
-        if (title) config.extensions.coloredLinks.color(this, title);
+    $('.button, .tiddlyLink', place).each(function (i) {
+        var btn = $(this),
+            title = btn.attr('tiddlyLink');
+        if (
+            !title && btn.hasClass('hastags') ||
+            btn.hasClass('hasnotags')
+        )
+            title = btn.attr('tag');
+
+        if (title)
+            me.color(this, title);
     });
 };
 
 
 Story.prototype.refreshTiddler_COLOR = Story.prototype.refreshTiddler;
 Story.prototype.refreshTiddler = function (title, template, force, customFields, defaultText) {
-    var el = Story.prototype.refreshTiddler_COLOR.apply(this, arguments),
-	c = config.extensions.coloredLinks;
-    if (!title) alert('foo');
-    if (c.colorTitles) c.color(jQuery(el).find(c.titleSelector)[0], title, true);
+    var el = Story.prototype.refreshTiddler_COLOR.apply(this, arguments);
+    //titles to be colored?
+    if (me.colorTitles)
+        //color it
+        me.color(
+            //the title
+            $(el).find(me.titleSelector)[0],
+            title,
+            true
+        );
     return el;
 };
+
+})(jQuery);
 //}}}
