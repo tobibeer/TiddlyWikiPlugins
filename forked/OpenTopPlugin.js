e200eb5ba9d673294d5d0c267002aab16591efb1
Tobias Beer <beertobias@gmail.com>
2013-10-07 22:28:23 +0200

    OpenTopPlugin 0.3.0 — fixed bug where opening a non-existing tiddler in editmode closed the editor

--- forked/OpenTopPlugin.js
+++ forked/OpenTopPlugin.js
@@ -2,7 +2,7 @@
 |''Name''|OpenTopPlugin|
 |''Author''|[[Tobias Beer|http://tobibeer.tiddlyspace.com]] / original by Saq Imtiaz|
 |''Description''|open tiddlers at the top of the screen|
-|''Version''|0.2|
+|''Version''|0.3 (2013-10-07)|
 |''Requires''|2.5.2|
 |''License''|CC BY-SA|
 |''Source''|https://raw.github.com/tobibeer/TiddlyWikiPlugins/master/forked/OpenTopPlugin.js|
@@ -12,7 +12,7 @@ Story.prototype.displayTiddlerOPENTOP = Story.prototype.displayTiddler;
 Story.prototype.displayTiddler =
 function (srcElement, tiddler, template, animate, unused, customFields, toggle,animationSrc) {
 	srcElement = null;
-	this.closeTiddler((tiddler instanceof Tiddler) ? tiddler.title : tiddler);
+	if(template!=2)this.closeTiddler((tiddler instanceof Tiddler) ? tiddler.title : tiddler);
 	//call default
 	this.displayTiddlerOPENTOP.apply(this, arguments);
 	window.scrollTo(0,0);
