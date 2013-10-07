74ee41a154673e7656ab7c84dd1f809c24682a16
Tobias Beer <beertobias@gmail.com>
2013-10-07 10:48:39 +0200

    FontSizePlugin 1.1.5 — fixed default selector / added minified

--- plugins/FontSizePlugin.js
+++ plugins/FontSizePlugin.js
@@ -1,9 +1,9 @@
 /***
 |Name|FontSizePlugin|
-|Created by|[[Tobias Beer|http://tobibeer.tiddlyspace.com]] / branched from [[Saq Imtiaz' code|http://tiddlywiki.squize.org/#FontSizePlugin]]|
+|Created by|[[Tobias Beer|http://tobibeer.tiddlyspace.com]] / fork of [[Saq Imtiaz' code|http://tiddlywiki.squize.org/#FontSizePlugin]]|
 |Source|https://raw.github.com/tobibeer/TiddlyWikiPlugins/master/plugins/FontSizePlugin.js|
 |Documentation|http://fontsize.tiddlyspace.com|
-|Version|1.1.0|
+|Version|1.1.5 (2013-10-07)|
 |Core|2.5.2|
 !Usage
 {{{
@@ -14,96 +14,106 @@
 ***/
 //{{{
 (function ($) {
-    config.fontsize = {
-        //where to apply fontsize => use '.tiddler .viewer' for tiddler contents only
-        selector: 'body',
 
-        //all sizes in %
-        defaultSize: 100,
-        maxSize: 200,
-        minSize: 40,
-        stepSize: 10,
+var co = config.options,
+    cfg = config.fontsize = {
+    //where to apply fontsize => use '.tiddler .viewer' for tiddler contents only
+    selector: '#contentWrapper',
 
-        //button labels
-        increase: '+',
-        decrease: '-',
-        reset: '=',
+    //all sizes in %
+    defaultSize: 100,
+    maxSize: 200,
+    minSize: 40,
+    stepSize: 10,
 
-        //button tooltips
-        tooltipIncrease: 'increase font-size',
-        tooltipDecrease: 'decrease font-size',
-        tooltipReset: 'reset font-size',
+    //button labels
+    increase: '+',
+    decrease: '-',
+    reset: '=',
 
-        //format for styles
-        fmtCSS: '%0 {font-size: %1%;}'
-    }
+    //button tooltips
+    tooltipIncrease: 'increase font-size',
+    tooltipDecrease: 'decrease font-size',
+    tooltipReset: 'reset font-size',
 
-    //shortcut
-    var cfg = config.fontsize;
+    //format for styles
+    fmtCSS: '%0 {font-size: %1%;}'
+}
 
-    //init option
-    if (!config.options.txtFontSize) {
-        //set to default
-        config.options.txtFontSize = cfg.defaultSize;
+//init option
+if (!co.txtFontSize) {
+    //set to default
+    co.txtFontSize = cfg.defaultSize;
+    //save cookie
+    saveOptionCookie('txtFontSize');
+}
+
+//the macro
+config.macros.fontsize = {
+    //the handler
+    handler: function (place, macroName, params, wikifier, paramString, tiddler) {
+        var controls = createTiddlyElement(place, "div", null, "font-controls");
+        if (params[0])
+            createTiddlyText(
+                createTiddlyElement(controls, "span", null, "font-controls-label"),
+                params[0]
+            );
+
+        createTiddlyButton(controls, cfg.increase, cfg.tooltipIncrease, this.set, 'button font-increase');
+        createTiddlyButton(controls, cfg.reset, cfg.tooltipReset, this.set, 'button font-reset');
+        createTiddlyButton(controls, cfg.decrease, cfg.tooltipDecrease, this.set, 'button font-decrease');
+        setStylesheet(cfg.selector + ' {font-size:' + co.txtFontSize + '%;}', 'StylesFontSize');
+    },
+
+    //sets the fontsize
+    set: function () {
+        var btn = $(this),
+            f = parseInt(co.txtFontSize);
+        if (btn.hasClass('font-increase')) {
+            if (f < cfg.maxSize)
+                co.txtFontSize = (f * 1) + cfg.stepSize;
+        }
+        if (btn.hasClass('font-decrease')) {
+            if (f > cfg.minSize)
+                co.txtFontSize = (f * 1) - cfg.stepSize;
+        }
+        if (btn.hasClass('font-reset')) {
+            co.txtFontSize = cfg.defaultSize;
+        }
         //save cookie
-        saveOptionCookie('txtFontSize');
+        saveOptionCookie("txtFontSize");
+        //update styles
+        setStylesheet(cfg.fmtCSS.format([cfg.selector, co.txtFontSize]), 'StylesFontSize');
     }
+}
 
-    //other styles
-    config.shadowTiddlers.StyleSheetFontSize =
-        '.font-controls {display:block;}\n' +
-        '#sidebarOptions .font-controls {float:right;clear:none;}' +
-        '#contentWrapper .font-controls-label {display:block;float:left;}\n' +
-        '#contentWrapper .font-controls .button {display:block; float:left; padding: 0 0.2em; margin:0 0.2em; width:1em; text-align:center !important}\n';
-
-    //update on changes
-    store.addNotification('StyleSheetFontSize', refreshStyles);
-
-    //the macro
-    config.macros.fontsize = {
-        //the handler
-        handler: function (place, macroName, params, wikifier, paramString, tiddler) {
-            var controls = createTiddlyElement(place, "span", null, "font-controls");
-            if (params[0])
-                createTiddlyText(
-                    createTiddlyElement(controls, "span", null, "font-controls-label"),
-                    params[0]
-                );
-
-            createTiddlyButton(controls, cfg.increase, cfg.tooltipIncrease, this.set, 'button font-increase');
-            createTiddlyButton(controls, cfg.reset, cfg.tooltipReset, this.set, 'button font-reset');
-            createTiddlyButton(controls, cfg.decrease, cfg.tooltipDecrease, this.set, 'button font-decrease');
-            setStylesheet(cfg.selector + ' {font-size:' + config.options.txtFontSize + '%;}', 'StylesFontSize');
-        },
-
-        //sets the fontsize
-        set: function () {
-            var btn = $(this);
-            if (btn.hasClass('font-increase')) {
-                if (config.options.txtFontSize < cfg.maxSize)
-                    config.options.txtFontSize = (config.options.txtFontSize * 1) + cfg.stepSize;
-            }
-            if (btn.hasClass('font-decrease')) {
-                if (config.options.txtFontSize > cfg.minSize)
-                    config.options.txtFontSize = (config.options.txtFontSize * 1) - cfg.stepSize;
-            }
-            if (btn.hasClass('font-reset')) {
-                config.options.txtFontSize = cfg.defaultSize;
-            }
-            //save cookie
-            saveOptionCookie("txtFontSize");
-            //update styles
-            setStylesheet(cfg.fmtCSS.format([cfg.selector, config.options.txtFontSize]), 'StylesFontSize');
-        }
+//startup paramifier 'font'
+config.paramifiers.font = {
+    onstart: function (size) {
+        //save and set font
+        co.txtFontSize = size;
+        config.macros.fontsize.set();
     }
+}
 
-    //startup paramifier 'font'
-    config.paramifiers.font = {
-        onstart: function (size) {
-            //save and set font
-            config.options.txtFontSize = size;
-            config.macros.fontsize.set();
-        }
-    }
+//other styles
+config.shadowTiddlers.StyleSheetFontSize = [
+'/*{{{*/',
+'#sidebarOptions .font-controls {',
+'float:right;clear:none;',
+'}',
+'#contentWrapper .font-controls-label {',
+'display:block;float:left;',
+'}',
+'#contentWrapper .font-controls .button {',
+'display:block; float:left;',
+'padding: 0 0.2em; margin:0;',
+'width:1em; text-align:center !important',
+'}',
+'/*}}}*/'].join('\n');
+
+//update on changes
+store.addNotification('StyleSheetFontSize', refreshStyles);
+
 })(jQuery);
 //}}}
