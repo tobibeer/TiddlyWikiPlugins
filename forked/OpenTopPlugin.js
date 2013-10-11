e7396b7e7dfe617b61396f08a2adc0488dd20c08
Tobias Beer <beertobias@gmail.com>
2013-10-11 12:25:09 +0200

    OpenTopPlugin 0.3.3 — prevent scrolling when entering edit mode

--- forked/OpenTopPlugin.js
+++ forked/OpenTopPlugin.js
@@ -2,7 +2,7 @@
 |''Name''|OpenTopPlugin|
 |''Author''|[[Tobias Beer|http://tobibeer.tiddlyspace.com]] / original by Saq Imtiaz|
 |''Description''|open tiddlers at the top of the screen|
-|''Version''|0.3.1 (2013-10-08)|
+|''Version''|0.3.3 (2013-10-10)|
 |''Requires''|2.5.2|
 |''License''|CC BY-SA|
 |''Source''|https://raw.github.com/tobibeer/TiddlyWikiPlugins/master/forked/OpenTopPlugin.js|
@@ -15,7 +15,7 @@ function (srcElement, tiddler, template, animate, unused, customFields, toggle,a
 	if(template!=2)this.closeTiddler((tiddler instanceof Tiddler) ? tiddler.title : tiddler);
 	//call default
 	var result = this.displayTiddlerOPENTOP.apply(this, arguments);
-	window.scrollTo(0,0);
+	if(template!=2)window.scrollTo(0,0);
 	return result;
 }
 /*}}}*/
