a744353623c95dbf6dd895d03ba456cfe1ce8ca9
Tobias Beer <beertobias@gmail.com>
2013-10-13 18:59:49 +0200

    PopupMacro 1.5.0 — Spaghetti code  be gone! + hover, sticky, toggle
    I have no excuses for having this piece of spaghetti code linger around
    for so long.
    
    Anyhow, the new features are now documented here...
    http://tobibeer.tiddlyspace.com/#Popup

--- plugins/PopupMacro.js
+++ plugins/PopupMacro.js
@@ -1,142 +1,297 @@
 ﻿/***
 |''Name:''|~PopupMacro|
-|''Author:''|Saq Imtiaz (mod: Tobias Beer)|
-|''Documentation:''|http://tobibeer.tiddlyspace.com/#PopupMacroMod|
-|''Version:''|1.3.1 TB (2013-09-05)|
-|''Description:''|Create popups with custom content|
+|''Description:''|create (nested) popups with custom content|
+|''Documentation:''|http://tobibeer.tiddlyspace.com/#Popup|
+|''Author:''|Tobias Beer (original author: Saq Imtiaz)|
+|''Version:''|1.5.0 (2013-10-13)|
+|''CoreVersion:''|2.5.3 or better|
 |''Source''|https://raw.github.com/tobibeer/TiddlyWikiPlugins/master/plugins/PopupMacro.js|
-|''Requires:''|TW Version 2.5.3 or better|
 !Code
 ***/
 //{{{
+
+(function($){
+
 //PARAMETERS
-config.macros.popup = {
+var me = config.macros.popup = {
+
+    //DEFAULTS
     hover: true,
-    hideAfter: 700,
-    showAfter: 200,
-    fadeTime: 300,
+    toggle: false,
+    sticky: false,
+    arrow: document.all ? " ▼" : " ▾",
+    hoverables: '#mainMenu',
+    showAfter: 400,
+    showStandardAfter: 800,
+    enableHover:'.button[tag], .tiddlyLink[tag]',
+    disableHover:'.slidr_button',
+
+    //LOCALISATION
+
     err1: 'missing macro parameters',
     err2: 'missing label or content parameter',
-    arrow: document.all ? " ▼" : " ▾",
 
-    //DO NOT CHANGE
+    //THE MACRO
     handler: function (place, macroName, params, wikifier, paramString, theTiddler) {
         var btn, lbl, src, delay, show,
-            p = paramString.parseParams(null, null, true),
-            id = getParam(p, 'id', 'nestedpopup'),
-            cls = 'popup popupmacro' + getParam(p, 'class', ''),
-            arr = getParam(p, 'arrow', this.arrow),
-            hover = (params.contains('hover') || this.hover) && (!params.contains('nohover') || !this.hover),
-            btnId = 'btn' + (new Date()).formatString('YYYYMMDDhhmmss') + Math.random().toString().substr(6);
+            //parse params
+            p = paramString.parseParams('anon', null, true),
+            //init params data object
+            d = {
+                //with content
+                content: (params[1] || '').replace(/(\$\)\)|\>\+\>)/g, ">>"),
+                //arrow
+                arrow: getParam(p, 'arrow', me.arrow)
+            },
+            //get custom class
+            cls = getParam(p, 'class', '');
 
-        if (!params[0] || !params[1]) { createTiddlyError(place, this.err1, this.err2); return false; }
+        //button label
         lbl = params[0];
-        src = (params[1]).replace(/\$\)\)/g, ">>");
 
-        btn = createTiddlyButton(
-            place,
-            lbl + arr,
-            lbl,
-            this.show,
-            'button popupbutton',
-            btnId,
-            null,
-            {
-                open: 'true',
-                popupclass: cls,
-                content: src
-            }
-        );
-        btn.setAttribute('popupid', !isNested(btn) ? id : 'popup');
-        btn.click = this.click;
-        btn.onmouseover = hover ? (this.showAfter ? this.delay : this.show) : null;
-        btn.onmouseout = hover && this.showAfter ? this.abort : null;
-    },
-    close: function (p) {
-        if (p.getAttribute('close') == 'ok') {
-            //jQuery(p).fadeOut(this.fadeTime);
-            //setTimeout(Popup.remove(),this.fadeTime);
+        //label or content missing?
+        if (!lbl || !d.content) {
+            //error and out
+            createTiddlyError(place, me.err1, me.err2);
+            return false;
         }
-    },
-    mouseout: function () {
-        var cmp = config.macros.popup, p = this;
-        p.setAttribute('close', 'ok');
-        setTimeout(function () { cmp.close(p); }, cmp.hideAfter);
-    },
-    mouseover: function () {
-        this.setAttribute('close', '');
+
+        //loop these params
+        ['sticky', 'toggle', 'hover'].map(function(param){
+            //determine named param value (fallback = default)
+            var val = getParam(p, param, me[param]);
+            //set data for param as...
+            d[param] =
+                //when 'nohover', etc...
+                params.contains('no' + param) ?
+                //to false
+                false :
+                //otherwise to named param or true when 'hover', etc...
+                (val || params.contains(param) );
+
+            //turn stringified booleans into boolean
+            if(val == 'true') d[param] = 1;
+            if(val == 'false') d[param] = 0;
+        });
+
+        if(params.contains('noarrow')) d.arrow = '';
+
+        //create popup button
+        btn = $(createTiddlyButton(
+            place,
+            lbl + d.arrow,
+            lbl,
+            d.toggle ? me.toggleButton : me.show,
+            'button popupbutton' + (cls ? ' ' + cls  : ''),
+            null,
+            null
+        ));
+
+        //determine popup class
+        d.popclass = 
+            'popup popupmacro' +
+                (me.inPopup(btn) ? ' nested' : '') +
+                (cls ? ' ' + cls : '');
+
+        //when hover is off, show by default
+        d.show = (!d.hover);
+
+        //remember settings
+        btn.data(d);
+
+        if(d.hover){
+            btn.on('mouseover', me.showAfter ? me.delay : me.show);
+            btn.on('mouseout', me.abort);
+        };
     },
     delay: function (e) {
-        var btn = this, cmp = config.macros.popup;
-        btn.setAttribute('abort', '');
-        setTimeout(function () { cmp.show(btn); }, cmp.showAfter);
+        var btn = $(this.innerHTML ? this : e);
+        btn.data('show', true);
+        setTimeout(
+            function () {
+                if(btn.data('show')){
+                    if(btn.is('.popupbutton'))
+                        me.show(btn[0])
+                    else
+                        btn.click();
+                }
+            },
+            btn.is('.popupbutton') ? me.showAfter : me.showStandardAfter
+        );
     },
     abort: function (e) {
-        var cmp = config.macros.popup;
-        this.setAttribute('abort', 'true');
-        var p = document.getElementById(this.getAttribute('popupid'));
-        if (p) {
-            p.setAttribute('close', 'ok');
-            setTimeout(function () { cmp.close(p); }, cmp.hideAfter);
-        }
+        $(this).data('show', false);
     },
-    click: function(e){
-        var btn = resolveTarget(e || window.event);
-        btn.setAttribute('open',true);
-        this.show(btn);
-        btn.setAttribute('open',false);        
+    toggleButton: function (e){
+        var btn = $(this),
+            p = btn.data('popup');
+
+        //popup already displayed?
+        if(p)
+            //close
+            Popup.removeFrom($(p).data('level'));
+        //popup not displayed?
+        else
+            //show it
+            me.show.apply(this, arguments);
     },
     show: function (e) {
         e = e || window.event;
-        var open, nest, p, tgt, cmp = config.macros.popup,
-            btn = this.innerHTML ? this : e,
-            btnId = btn.getAttribute('id'),
-            id = btn.getAttribute('popupid'),
-            cls = btn.getAttribute('popupclass'),
-            src = btn.getAttribute('content');
+        var p,
+            level = Popup.stack.length,
+            btn = $(this.innerHTML ? this : e),
+            inPopup = me.inPopup(btn),
+            popLevel = inPopup ? btn.closest('.popup').data('level') : 0;
 
-        if (btn.getAttribute('abort') == 'true') return;
-        open = btn.getAttribute('open') == 'true';
-        btn.setAttribute('open', !open);
-        if (open) {
-            tgt = this.innerHTML ? resolveTarget(e || window.event) : btn;
-            if (nest && Popup.stack.length > 1) Popup.removeFrom(1);
-            else if (!nest && Popup.stack.length > 0) Popup.removeFrom(0);
-            p = createTiddlyElement(document.body, "ol", id, cls, null);
-            p.onmouseover = cmp.mouseover;
-            p.onmouseout = cmp.mouseout;
-            Popup.resetButtonShow();
-            Popup.showButtonId = btnId;
-            Popup.stack.push({ root: btn, popup: p });
-            wikify(src, p);
-            Popup.show(p, true);
-            if (e) {
-                e.cancelBubble = true;
-                if (e.stopPropagation) e.stopPropagation();
+        //when not already open
+        if (!btn.data('popup')) {
+
+            if (!inPopup && level)
+                Popup.removeFrom(0);
+            else if (inPopup && level > 1){
+                level = popLevel + 1;
+                Popup.removeFrom(level);
             }
-            return false;
+
+            p = createTiddlyElement(
+                document.body,
+                "ol",
+                null,
+                btn.data('popclass') + (level ? ' nested' + (level) : '') ,
+                null
+            );
+
+            btn.data('popup',p).addClass('popupopen');
+
+            $(p).data({
+                button:btn,
+                level:level,
+                sticky:btn.data('sticky')
+            }).click(me.popupClick);
+
+            Popup.stack.push({
+                root: btn[0],
+                popup: p
+            });
+
+            wikify(btn.data('content'), p);
+            Popup.show(p, true);
+        }
+
+        if (e) {
+            e.cancelBubble = true;
+            if (e.stopPropagation) e.stopPropagation();
+        }
+        return false;
+    },
+
+    popupClick: function(e){
+        var e = e || window.event,
+            //get popup
+            p = $(this).closest('.popup'),
+            //get clicked element
+            tgt = resolveTarget(e),
+            //when link
+            linkClicked = $(tgt).closest('a').length;
+
+        //remove popups of deeper levels
+        Popup.removeFrom(p.data('level')+1);
+
+        //except when link clicked
+        if(linkClicked && !p.data('sticky')){
+            //close popup 
+            Popup.remove();
+        //otherwise
+        }else{
+            e.cancelBubble = true;
+            if (e.stopPropagation) e.stopPropagation();
+        }
+        return false;
+    },
+
+    //checks if an element is inside a popup
+    inPopup: function (el) {
+        return $(el).closest('.popup').length;
+    },
+
+    linkOver: function(e){
+        var a = $(this),
+            level = a.closest('.popup').data('level') || 0;
+
+        //close
+        Popup.removeFrom(level);
+        //when
+        if(
+            (
+                //enabled or has tiddler list (groupBy)
+                a.is(me.enableHover) || a.data('tiddlers')
+            ) && !(
+                //when disabled
+                a.is(me.disableHover)
+            )
+        //trigger opening after delay
+        ) me.delay(a);
+    },
+
+    makeHoverable : function(el){
+        var a = $(el);
+
+        //find links in any outer "hoverable" container
+        if(
+            a.is('a') &&
+            a.closest(me.hoverables).length &&
+            !a.is('.popupbutton')
+        ){
+            //a mouseover function to delay opening the popup
+            a.off('mouseover', me.linkOver);
+            a.on('mouseover', me.linkOver);
+            //don't open the popup anymore (unless it already is)
+            a.off('mouseout', me.abort);
+            a.on('mouseout', me.abort);
         }
     }
 }
 
-window.isNested = function (el) {
-    var c = document.getElementById("contentWrapper");
-    while (el != null) { if (el == c) return true; el = el.parentNode; } return false;
+window.refreshElementsPOPUP = window.refreshElements,
+window.refreshElements = function(root,changeList){
+    window.refreshElementsPOPUP.apply(this,arguments);
+    var el = $(root);
+    $('a', el).add(el).each(function(){
+        me.makeHoverable(this)
+    });    
 }
 
-Popup.resetButtonShow = function () {
-    var btn = document.getElementById(Popup.showButtonId);
-    if (btn) {
-        btn.setAttribute('open', 'true');
-        Popup.showButtonId = '';
+window.invokeMacroPOPUP = window.invokeMacro;
+window.invokeMacro = function invokeMacro(place,macro,params,wikifier,tiddler){
+    window.invokeMacroPOPUP.apply(this,arguments);
+    var last = $(place.lastChild);
+    $('a', last).add(last).each(function(){
+        me.makeHoverable(this)
+    });
+}
+
+Popup.removeFromPOPUP = Popup.removeFrom;
+Popup.removeFrom = function(from){
+    var btn, p, t;
+    //loop popups to be removed
+    for(t=Popup.stack.length-1; t>=from; t--) {
+        //get popup
+        p = Popup.stack[t],
+        //with root element
+        $(p.root)
+            //remove level and popup reference
+            .removeData('popup')
+            //remove open class
+            .removeClass('popupopen');
     }
-}
-
-if (!Popup.macroRemove) Popup.macroRemove = Popup.remove;
-Popup.remove = function (pos) {
-    Popup.resetButtonShow();
-    Popup.macroRemove.apply(this, arguments);
+    Popup.removeFromPOPUP.apply(this,arguments);
 };
 
-setStylesheet('#nestedpopup {margin-left:1em;}.popupbutton{cursor:pointer};', 'PopupMacroStyles');
+setStylesheet(
+    '.nested {padding:3px;margin:-0.5em 0 0 2em !important;}'+
+    '.popupbutton {cursor:pointer};',
+    'PopupMacroStyles'
+);
+
+})(jQuery)
 //}}}
