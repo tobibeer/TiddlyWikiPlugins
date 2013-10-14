a154aab4db6af5e4a9deb7e6dbb35515256667e9
Tobias Beer <beertobias@gmail.com>
2013-10-14 15:30:32 +0200

    OpenTopPlugin 0.3.4 — instead of reaopening, puts already open tiddler at top

--- forked/OpenTopPlugin.js
+++ forked/OpenTopPlugin.js
@@ -2,20 +2,28 @@
 |''Name''|OpenTopPlugin|
 |''Author''|[[Tobias Beer|http://tobibeer.tiddlyspace.com]] / original by Saq Imtiaz|
 |''Description''|open tiddlers at the top of the screen|
-|''Version''|0.3.3 (2013-10-10)|
+|''Version''|0.3.4 (2013-10-14)|
 |''Requires''|2.5.2|
 |''License''|CC BY-SA|
 |''Source''|https://raw.github.com/tobibeer/TiddlyWikiPlugins/master/forked/OpenTopPlugin.js|
 ***/
 /*{{{*/
+(function($){
+
 Story.prototype.displayTiddlerOPENTOP = Story.prototype.displayTiddler;
 Story.prototype.displayTiddler =
 function (srcElement, tiddler, template, animate, unused, customFields, toggle,animationSrc) {
 	srcElement = null;
-	if(template!=2)this.closeTiddler((tiddler instanceof Tiddler) ? tiddler.title : tiddler);
+	//get already open tiddler
+	var tid = story.getTiddler((tiddler instanceof Tiddler) ? tiddler.title : tiddler);
+	//if not edit mode and open => make first
+	if(template!=2 && tid) $('#tiddlerDisplay').prepend(tid);
 	//call default
 	var result = this.displayTiddlerOPENTOP.apply(this, arguments);
-	if(template!=2)window.scrollTo(0,0);
+	//scroll top, except when in edit mode
+	if(template!=2) window.scrollTo(0,0);
 	return result;
 }
+
+})(jQuery)
 /*}}}*/
